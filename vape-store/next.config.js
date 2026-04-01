106cd459a405ce9674b97a1b8cb6b2d42d47ae25
ayushmane04 <109031326+ayushmane04@users.noreply.github.com>
2026-04-01 17:18:11 +0530

    Update next.config.js

--- vape-store/next.config.js
+++ vape-store/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'linen-hamster-222394.hostingersite.com'],
     remotePatterns: [
       {
         protocol: 'https',
